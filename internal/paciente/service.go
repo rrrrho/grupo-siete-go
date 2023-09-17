2a5d2702f9d34c22c71375320b51853fb7ab7553
Nicolas Gambino <nicogambino13@gmail.com>
2023-09-17 10:31:36 -0300

    creado el repository, service y handler agregar campo id al modelo paciente

---
+++ internal/paciente/service.go
@@ -0,0 +1,72 @@
+package paciente
+
+import "errors"
+
+type Service struct {
+	repository RepositoryInterface
+}
+
+func NewService(repository RepositoryInterface) *Service {
+	return &Service{repository: repository}
+}
+
+type ServiceInterface interface {
+	GetByID(id int) (Paciente, error)
+	Save(pacienteInput Paciente) (Paciente, error)
+	Update(id int, pacienteInput Paciente) (Paciente, error)
+	Delete(id int) error
+}
+
+var (
+	ErrPacienteNotFound = errors.New("paciente not found")
+	ErrSavePaciente     = errors.New("error saving paciente")
+	ErrUpdatePaciente   = errors.New("error updating paciente")
+	ErrDeletePaciente   = errors.New("error deleting paciente")
+)
+
+func (s *Service) GetByID(id int) (Paciente, error) {
+	paciente, err := s.repository.GetByID(id)
+	if err != nil {
+		return Paciente{}, ErrPacienteNotFound
+	}
+	return paciente, nil
+}
+
+func (s *Service) Save(pacienteInput Paciente) (Paciente, error) {
+	paciente, err := s.repository.Save(pacienteInput)
+	if err != nil {
+		return Paciente{}, ErrSavePaciente
+	}
+	return paciente, nil
+}
+
+func (s *Service) Update(id int, pacienteInput Paciente) (Paciente, error) {
+	pacienteToUpdate := Paciente{}
+	if pacienteInput.Nombre != "" {
+		pacienteToUpdate.Nombre = pacienteInput.Nombre
+	}
+	if pacienteInput.Apellido != "" {
+		pacienteToUpdate.Apellido = pacienteInput.Apellido
+	}
+	if pacienteInput.Domicilio != "" {
+		pacienteToUpdate.Domicilio = pacienteInput.Domicilio
+	}
+	if pacienteInput.DNI != "" {
+		pacienteToUpdate.DNI = pacienteInput.DNI
+	}
+	if !pacienteInput.FechaDeAlta.IsZero() {
+		pacienteToUpdate.FechaDeAlta = pacienteInput.FechaDeAlta
+	}
+	paciente, err := s.repository.Modify(id, pacienteToUpdate)
+	if err != nil {
+		return Paciente{}, ErrUpdatePaciente
+	}
+	return paciente, nil
+}
+func (s *Service) Delete(id int) error {
+	err := s.repository.Delete(id)
+	if err != nil {
+		return ErrDeletePaciente
+	}
+	return nil
+}
